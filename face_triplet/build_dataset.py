ca684793f405ee268e4aea532e1510770c05dfd7
drDrozio <ishanss2018@gmail.com>
2021-11-15 17:44:43 +0530

    start file updated

--- face_triplet/build_dataset.py
+++ face_triplet/build_dataset.py
@@ -4,6 +4,7 @@ from imutils.video import VideoStream
 import time
 import cv2
 import os
+import json
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -24,43 +25,74 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 total = 0
 
-# # Logic to register new users
-# name = input("Enter name : ")
-# os.path.isfile(path)
+def add_user():
+	# Logic to register new users
+	name = input("Enter name : ")
+	category = input("Enter category : ")
+	# Parent Directory path
+	parent_dir = "./dataset/"
+	# Path
+	path = os.path.join(parent_dir, name)
+	os.mkdir(path)
+	print("Directory '% s' created" % name)
 
-# loop over the frames from the video stream
-while True:
-	# grab the frame from the threaded video stream, clone it, (just
-	# in case we want to write it to disk), and then resize the frame
-	# so we can apply face detection faster
-	frame = vs.read()
-	orig = frame.copy()
-	frame = imutils.resize(frame, width=400)
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(
-		cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30))
-	# loop over the face detections and draw them on the frame
-	for (x, y, w, h) in rects:
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	if os.path.isfile(path):
+		print("User exists. Try again.")
+	else:
+		mappath = 'users_register'
+		if not os.path.isfile(mappath):
+			data = {}
+			data['categories']
+			data['categories'].append({
+				name : category
+			})
+			with open('users_register.txt', 'w') as outfile:
+				json.dump(data, outfile)
+		else:
+			with open('users_register.txt', 'w') as outfile:
+				data['categories']
+				data['categories'].append({
+					name : category
+				})
+    			json.dump(data, outfile)
+		# loop over the frames from the video stream
+		while True:
+			# grab the frame from the threaded video stream, clone it, (just
+			# in case we want to write it to disk), and then resize the frame
+			# so we can apply face detection faster
+			frame = vs.read()
+			orig = frame.copy()
+			frame = imutils.resize(frame, width=400)
+			# detect faces in the grayscale frame
+			rects = detector.detectMultiScale(
+				cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
+				minNeighbors=5, minSize=(30, 30))
+			# loop over the face detections and draw them on the frame
+			for (x, y, w, h) in rects:
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the `k` key was pressed, write the *original* frame to disk
-	# so we can later process it and use it for face recognition
-	if key == ord("k"):
-		p = os.path.sep.join([args["output"], "{}.png".format(
-			str(total).zfill(5))])
-		cv2.imwrite(p, orig)
-		total += 1
-	# if the `q` key was pressed, break from the loop
-	elif key == ord("q"):
-		break
-    
-# print the total faces saved and do a bit of cleanup
-print("[INFO] {} face images stored".format(total))
-print("[INFO] cleaning up...")
-cv2.destroyAllWindows()
-vs.stop()
+			# show the output frame
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+
+			# if the `k` key was pressed, write the *original* frame to disk
+			# so we can later process it and use it for face recognition
+			if key == ord("k"):
+				p = os.path.sep.join([args["output"], "{}.png".format(
+					str(total).zfill(5))])
+				cv2.imwrite(p, orig)
+				total += 1
+			# if the `q` key was pressed, break from the loop
+			elif key == ord("q"):
+				break
+			
+		# print the total faces saved and do a bit of cleanup
+		print("[INFO] {} face images stored".format(total))
+		print("[INFO] cleaning up...")
+		cv2.destroyAllWindows()
+		vs.stop()
+
+
+		
+
+
--- start.py
+++ start.py
@@ -1,2 +1,25 @@
-from Hand_Gesture.capstone_hand_ges import handgesture
-from face_triplet.recognize_video import 
+from Hand_Gesture import handgesture
+from face_triplet.recognize_video import facerecog
+
+
+
+'''
+    facerecog()
+        input : 
+        output : name , confidence
+
+    handgesture()
+        input : 
+        output : list
+'''
+def start():
+    # Get User_Register Dictionary
+    data = {}
+    # Face Module
+    name=facerecog()
+    # user category determination
+    category=data['category'][name]
+
+    # Hand Module
+    handgesture()
+    # Latch Circuit
